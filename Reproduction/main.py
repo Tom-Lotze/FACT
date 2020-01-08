8d255d0ca1980366a876efd93cc2a1e8aff23fb2
Tom Lotze <tom.lotze@student.auc.nl>
2020-01-08 12:51:34 +0100

    added imports

--- Reproduction/main.py
+++ Reproduction/main.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 # python 3
-# @Author: TomLotze
+# @Authors: Tom Lotze, Berend Jansen
 # @Date:   2020-01-08 12:16:10
 # @Last Modified by:   TomLotze
-# @Last Modified time: 2020-01-08 12:16:14
+# @Last Modified time: 2020-01-08 12:47:51
+
+# imports
+from __future__ import division, print_function, absolute_import
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+
+# import helper functions
+from autoenc_helpers import makedirs, list_of_distances, print_and_write, list_of_norms
+from data_preprocessing import batch_elastic_transform
